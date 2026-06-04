ae8279129d9c091f3bcfacc070dc9cdf415c6692
AHMAD OMAR ABUSHARIAH <133152734+Ahmad-Abbadi@users.noreply.github.com>
2026-06-04 14:23:38 +0300

    Add features to the Door

--- Assets/_project/Scripts/Door/Door.cs
+++ Assets/_project/Scripts/Door/Door.cs
@@ -4,6 +4,8 @@ public class Door : MonoBehaviour
 {
     [SerializeField] private KeyTypes doorKeyType;
     [SerializeField] private float openYRotation = 90f;
+    [SerializeField] private bool openToRight = true;
+    [SerializeField] private bool openToOutside = true;
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -13,11 +15,19 @@ public class Door : MonoBehaviour
         }
     }
 
-    [ContextMenu("Open Door")]
     private void TryOpenDoor()
     {
-            if (!PlayerInventory.Instance.TryGetKey(doorKeyType)) return;
+        if (!PlayerInventory.Instance.TryGetKey(doorKeyType)) return;
 
-            transform.Rotate(0f, openYRotation, 0f);
+        OpenDoor();
+    }
+
+    [ContextMenu("Open Door")]
+    private void OpenDoor()
+    {
+        float rightOrLeft = openToRight ? 1f : -1f;
+        float insideOrOutside = openToOutside ? 1f : -1f;
+
+        transform.Rotate(0f, openYRotation * rightOrLeft * insideOrOutside, 0f);
     }
 }
