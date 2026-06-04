be931ac910a26e7e7763635a6055da5788ea97d1
Ghadeen Yaseen <158176418+GhadeenYaseen@users.noreply.github.com>
2026-06-04 15:29:29 +0300

    update key ui on key inventory

--- Assets/_project/Scripts/inventory/PlayerInventory.cs
+++ Assets/_project/Scripts/inventory/PlayerInventory.cs
@@ -5,6 +5,8 @@ public class PlayerInventory : MonoBehaviour
 {
     public static PlayerInventory Instance { get; private set; }
 
+    //index0=bronze // index1=silver // index2=gold
+    [SerializeField] private List<GameObject> keyUIs = new List<GameObject>();
     private List<KeyTypes> collectedKeys = new List<KeyTypes>();
 
     private void Awake()
@@ -30,7 +32,10 @@ public class PlayerInventory : MonoBehaviour
         if (!collectedKeys.Contains(key))
         {
             collectedKeys.Add(key);
+            UpdateKeyUI(key, true);
+            
             Debug.Log("Key added: " + key);
+            
         }
     }
 
@@ -39,10 +44,29 @@ public class PlayerInventory : MonoBehaviour
         if (collectedKeys.Contains(keyType))
         {
             collectedKeys.Remove(keyType);
+            UpdateKeyUI(keyType, false);
+            
             Debug.Log("Key used: " + keyType);
             return true;
         }
 
         return false;
+    }
+
+    private void UpdateKeyUI(KeyTypes keyType, bool state)
+    {
+        switch (keyType)
+        {
+            case KeyTypes.Bronze: keyUIs[0].SetActive(state);
+            break;
+
+            case KeyTypes.Silver: keyUIs[1].SetActive(state);
+            break;
+            case KeyTypes.Gold: keyUIs[2].SetActive(state);
+            break;
+            
+            default: Debug.Log("oh nine u no key");
+            break;
+        }
     }
 }
